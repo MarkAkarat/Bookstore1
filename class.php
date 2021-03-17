954611fd2395ab2f1ffc3cd5a1f836f07ee8671b
MarkAkarat <6306021412091@fitm.kmutnb.ac.th>
2021-03-17 15:12:35 +0700

    update

--- class.php
+++ class.php
@@ -1,3 +1,21 @@
 <?php
-say yes
+class Database{
+    public $dbCon = null;
+    public function connect(){
+        define("host","localhost");
+        define("username","root");
+        define("password","");
+        define("database","bookstore");
+        $this->dbCon = new mysqli(host,username,password,database);
+        if($this->dbCon->connect_error){
+            die("Database Connection Error,Error No.:" .
+            $this->dbCon->connect_errno . " | " .
+            $this->dbCon->connect->connect_error);
+            $this->dbCon->query("SET NAMES UTF8");
+        }
+    }
+    public function disconnect(){
+        $this->dbCon->close();
+    }
+}
 ?>
